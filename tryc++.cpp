547bedf5260b1dd75f212dea3f7c5d3e4e2889ec
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2021-06-13 06:31:16 +0530

    push to the master

--- tryc++.cpp
+++ tryc++.cpp
@@ -1,14 +1,21 @@
 #include<iostream>
+#include <ostream>
 #include <stdio.h>
+using namespace std;
 int add(int a,int b){
     int c=0;
     c = a+b;
+    c = 3;
+    int newvar;
+    newvar = 5;
+    int newvar2;
+    newvar =newvar2;
     //c=master;
     return c;
 }
 int main()
 {
-    std::cout<<"Hello World";
-    std::cout<<add(2,3);
+    cout<<"Hello World\n";
+    cout<<add(2,3)<<endl;
     return 0;
 }
